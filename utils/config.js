ac3b9db36ba10592ceba93d5396f7b7a3a6fef43
Truman Cheung <>
2020-11-20 09:47:24 +0800

    move apikey

--- utils/config.js
+++ utils/config.js
@@ -5,13 +5,13 @@ export const IEX_SANDBOX_BASE_URL = 'https://sandbox.iexapis.com/stable';
 export const TDA_BASE_URL = 'https://api.tdameritrade.com/v1';
 export const TIINGO_BASE_URL = 'https://api.tiingo.com/iex';
 
-export const NEWS_API_KEY = 'cf74bbcf13fb4c998cc9c487c6acfc85';
-export const FINNHUB_API_KEY = 'bsdev0vrh5r8dht9873g';
-export const IEX_API_KEY = 'pk_9c3fe20063a14458a236fbba151b3523';
-export const IEX_SANDBOX_API_KEY = 'Tsk_9b260400520a4d23abfe1ef6cb0d3feb';
-export const TDA_CLIENT_ID = '1AQTLQTIRXA05MXOGHCE96NC5BFRTQFH';
+export const NEWS_API_KEY = process.env.NEWS_API_KEY;
+export const FINNHUB_API_KEY = process.env.FINNHUB_API_KEY;
+export const IEX_API_KEY = process.env.IEX_API_KEY;
+export const IEX_SANDBOX_API_KEY = process.env.IEX_SANDBOX_API_KEY;
+export const TDA_CLIENT_ID = process.env.TDA_CLIENT_ID;
 
 export const TDA_LOGIN_URL =
   'https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https%3A%2F%2Fchmtdsapi.herokuapp.com%2Fauth&client_id=1AQTLQTIRXA05MXOGHCE96NC5BFRTQFH%40AMER.OAUTHAP';
 
-export const TIINGO_API_KEY = "45a7c1bfaff6a70f9ed2ee2f3b9a10ecb4759a42";
+export const TIINGO_API_KEY = process.env.TIINGO_API_KEY;
