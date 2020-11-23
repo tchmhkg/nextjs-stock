852e1ea9f99640913ebb13f05e5a27fa291cdeb1
Truman Cheung <>
2020-11-23 12:00:17 +0800

    add progress bar module

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,26 @@
-import "~/styles/global.scss";
-import { LanguageProvider } from "~/context/LanguageContext";
-import ThemeManager from "~/theme";
+import 'nprogress/nprogress.css';
+import '~/styles/global.scss';
+import dynamic from 'next/dynamic';
+import { LanguageProvider } from '~/context/LanguageContext';
+import ThemeManager from '~/theme';
+
+const TopProgressBar = dynamic(
+  () => {
+    return import('~/components/top-progress-bar');
+  },
+  { ssr: false }
+);
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeManager>
-      <LanguageProvider lang={pageProps.localization?.locale}>
-        <Component {...pageProps} />
-      </LanguageProvider>
-    </ThemeManager>
+    <>
+      <TopProgressBar />
+      <ThemeManager>
+        <LanguageProvider lang={pageProps.localization?.locale}>
+          <Component {...pageProps} />
+        </LanguageProvider>
+      </ThemeManager>
+    </>
   );
 };
 
