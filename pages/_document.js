bd708da8891c7ad9e2ac52a4c5e0d3df41dca2fe
Truman Cheung <>
2020-11-25 14:31:28 +0800

    - fix className mismatch bug - remove unused hook

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,19 @@
 import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles';
+import flush from 'styled-jsx/server';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+    const materialUiSheets = new MaterialUiServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+            sheet.collectStyles(materialUiSheets.collect(<App {...props} />)),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -20,6 +23,8 @@ export default class MyDocument extends Document {
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+            {materialUiSheets.getStyleElement()}
+            {flush() || null}
           </>
         ),
       };
