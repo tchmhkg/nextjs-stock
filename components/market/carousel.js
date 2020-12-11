f6e6d2610cac9b92e6e2b9eb98c053ddf5a440b8
Truman Cheung <>
2020-12-11 21:10:58 +0800

    test

--- components/market/carousel.js
+++ components/market/carousel.js
@@ -3,32 +3,33 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 85px;
-  width: 100vw;
   position: sticky;
   position: -webkit-sticky;
   top: 70px;
-  bottom: 0;
   background-color: ${props => props.theme.background};
   -webkit-transition: background-color 200ms linear;
   -ms-transition: background-color 200ms linear;
   transition: background-color 200ms linear;
   z-index: 10;
   margin: 0 -15px;
+  overflow: hidden;
   ${'' /* overflow-x: auto; */}
   transform: translate3d(0,0,0);
 
 `;
 
 const Wrapper = styled.div`
-  ${'' /* overflow-x: auto; */}
   display: flex;
   align-items: center;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
+  flex: 1;
   -webkit-overflow-scrolling: touch;
+  overflow-x: auto;
+  position: relative;
+  ${'' /* -ms-overflow-style: none;
+  scrollbar-width: none;
   &::-webkit-scrollbar {
     display: none;
-  }
+  } */}
 `;
 
 const Carousel = ({ children }) => {
