2a1fddf1c4475ecfcb17a8ee24a0c08f8aaff8f5
Truman Cheung <trumancheunghm@gmail.com>
2020-12-12 21:26:16 +0800

    update

--- next.config.js
+++ next.config.js
@@ -54,10 +54,7 @@ module.exports = withPlugins(
           return config
         },
         env: {
-            NEWS_API_KEY: process.env.NEWS_API_KEY,
             FINNHUB_API_KEY: process.env.FINNHUB_API_KEY,
-            IEX_API_KEY: process.env.IEX_API_KEY,
-            IEX_SANDBOX_API_KEY: process.env.IEX_SANDBOX_API_KEY,
             TDA_CLIENT_ID: process.env.TDA_CLIENT_ID,
             TIINGO_API_KEY: process.env.TIINGO_API_KEY
         },
